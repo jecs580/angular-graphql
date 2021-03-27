db42701b253345583272bdd334b305de148c5d9c
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-03-26 21:01:34 -0400

    Creacion del header con navegacion a las rutas principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,54 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home.module').then(m => m.HomeModule) }, { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) }, { path: 'not', loadChildren: () => import('./components/pages/fotFound/not-found/not-found.module').then(m => m.NotFoundModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/charactes-list/charactes-list.module').then(m => m.CharactesListModule) }, { path: 'character-details', loadChildren: () => import('./components/pages/characters/charactes-details/charactes-details.module').then(m => m.CharactesDetailsModule) }, { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) }];
+const routes: Routes = [
+  {
+    path :'', redirectTo:'character-list', pathMatch:'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/pages/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'episodes',
+    loadChildren: () =>
+      import('./components/pages/episodes/episodes.module').then(
+        (m) => m.EpisodesModule
+      ),
+  },
+  {
+    path: 'character-list',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/charactes-list/charactes-list.module'
+      ).then((m) => m.CharactesListModule),
+  },
+  {
+    path: 'character-details',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/charactes-details/charactes-details.module'
+      ).then((m) => m.CharactesDetailsModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./components/pages/about/about/about.module').then(
+        (m) => m.AboutModule
+      ),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./components/pages/fotFound/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HeaderModule } from './shared/components/header/header.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
