634ccdd6611882664961dc90d5ba796edf59f5a8
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-03-29 23:40:49 -0400

    Estilos y contenido a la pagina de episodios

--- src/app/components/pages/episodes/episodes.component.ts
+++ src/app/components/pages/episodes/episodes.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { DataService } from './../../../shared/services/data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
   styleUrls: ['./episodes.component.scss']
 })
-export class EpisodesComponent implements OnInit {
-
-  constructor() { }
+export class EpisodesComponent {
+  episodes$ = this.dataService.episodes$;
+  constructor(private dataService:DataService ) { }
 
   ngOnInit(): void {
   }
