a20f41d16af69dbe1c4c3932a858490918de21c9
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-07 22:06:18 -0400

    Creacion de archivos para crear un snipper usando interceptors

---
+++ src/app/shared/components/snipper/snipper.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SnipperComponent } from './snipper.component';
+
+
+
+@NgModule({
+  declarations: [SnipperComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SnipperModule { }
